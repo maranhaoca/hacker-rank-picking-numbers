181abc45e947043d058234179b94ede0a409bacb
maranhaoca <maranhao.ca@gmail.com>
2022-10-28 10:39:59 +0100

    solve challenge

--- Tests/ResultTest.cs
+++ Tests/ResultTest.cs
@@ -38,15 +38,4 @@ public class ResultTest
 
         Assert.Equal(expected, actual);
     }
-
-    [Fact]
-    public void Test4()
-    {
-        var a = new List<int> { 4, 6, 5, 3, 3, 1 };
-
-        int expected = 22;
-        int actual = Result.pickingNumbers(a);
-
-        Assert.Equal(expected, actual);
-    }
 }
